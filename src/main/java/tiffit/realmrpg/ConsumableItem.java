1cabfe9dac5876230429e07a9b0d99829850548e
Richard Li <diamondspell13@gmail.com>
2017-03-10 21:47:51 -0600

    Revert "Stats and Items"
    This reverts commit ffe56e4a1b816db8dc121b234aeb197901df2bf5.

--- src/main/java/tiffit/realmrpg/ConsumableItem.java
+++
@@ -1,8 +0,0 @@
-package tiffit.realmrpg;
-
-public class ConsumableItem extends Item {
-	
-	public ConsumableItem(String name, int id, String[] args) {
-		super("consumable", name, id, args);
-	}
-}
