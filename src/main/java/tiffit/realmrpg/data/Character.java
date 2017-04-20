06a5ec9e595faae5701801a3a81370dc747cfb5f
Richard Li <diamondspell13@gmail.com>
2017-04-20 14:17:10 -0500

    start to implement exp system

--- src/main/java/tiffit/realmrpg/data/Character.java
+++ src/main/java/tiffit/realmrpg/data/Character.java
@@ -20,14 +20,32 @@ public class Character {
 	}
 	
 	public final ClassEnum clss;
+	
 	public int equip;
 	public int ability;
 	public int armor;
 	public int ring;
 	public int[] inventory = new int[8];
 	
-	public Character(ClassEnum clss) {
+	public int exp;
+	
+	public Character(ClassEnum clss, int exp) {
 		this.clss = clss;
+		this.exp = exp;
 	}
-
+	
+	public int getExp() {
+		return this.exp;
+	}
+	
+	public int getLevel() {
+		int levelExp = 0;
+		for (int i = 0; i < 20; i++) {
+			levelExp += i*100 + 50;
+			if (this.exp < levelExp) {
+				return i+1;
+			}
+		}
+		return 20;
+	}
 }
