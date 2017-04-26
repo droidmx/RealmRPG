989e26ac4c4a03f90636ec0fa51bde3ef13671e9
Richard Li <diamondspell13@gmail.com>
2017-04-26 14:15:00 -0500

    ?

--- src/main/java/tiffit/realmrpg/encounters/EncounterData.java
+++ src/main/java/tiffit/realmrpg/encounters/EncounterData.java
@@ -3,32 +3,26 @@ package tiffit.realmrpg.encounters;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
-public class Enemy {
+public class EncounterData {
 	
 	public String name;
-	public int hp;
-	public int def;
-	public int exp;
+	public EnemyData[] enemies;
 	
-	public static Enemy getData(String name){
-		File enemyFolder = new File("./data/enemies");
-		enemyFolder.mkdirs();
-		File enemyFile = new File(enemyFolder, name + ".json");
+	public static EncounterData getData(String name){
+		File encounterFolder = new File("./data/encounters");
+		encounterFolder.mkdirs();
+		File encounterFile = new File(encounterFolder, name + ".json");
 		
-		if(enemyFile.exists()){
+		if(encounterFile.exists()){
 			Gson gson = new GsonBuilder().create();
 			try {
-				return gson.fromJson(new FileReader(enemyFile), Enemy.class);
+				return gson.fromJson(new FileReader(encounterFile), EncounterData.class);
 			} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
 				e.printStackTrace();
 				return null;
--- src/main/java/tiffit/realmrpg/encounters/EnemyData.java
+++ src/main/java/tiffit/realmrpg/encounters/EnemyData.java
@@ -3,26 +3,32 @@ package tiffit.realmrpg.encounters;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
-public class Encounter {
+public class EnemyData {
 	
 	public String name;
-	public Enemy[] enemies;
+	public int hp;
+	public int def;
+	public int exp;
 	
-	public static Encounter getData(String name){
-		File encounterFolder = new File("./data/encounters");
-		encounterFolder.mkdirs();
-		File encounterFile = new File(encounterFolder, name + ".json");
+	public static EnemyData getData(String name){
+		File enemyFolder = new File("./data/enemies");
+		enemyFolder.mkdirs();
+		File enemyFile = new File(enemyFolder, name + ".json");
 		
-		if(encounterFile.exists()){
+		if(enemyFile.exists()){
 			Gson gson = new GsonBuilder().create();
 			try {
-				return gson.fromJson(new FileReader(encounterFile), Encounter.class);
+				return gson.fromJson(new FileReader(enemyFile), EnemyData.class);
 			} catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
 				e.printStackTrace();
 				return null;
